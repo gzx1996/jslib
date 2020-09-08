b82da7c5355da21beba1d1d11646179ef523c8f9
高振翔_time <xiangz.gao@timemicro.tech>
2020-09-08 10:39:21 +0800

    up

--- obj.js
+++ obj.js
@@ -66,7 +66,29 @@ module.exports = {
         return true;
       })
     );
+  },
+  /**
+   * obj扁平化
+   * @param {object} obj 
+   */
+  unflattenObject (obj) {
+    return Object.keys(obj).reduce((acc, k) => {
+      if (k.indexOf('.') !== -1) {
+        const keys = k.split('.');
+        Object.assign(
+          acc,
+          JSON.parse(
+            '{' +
+              keys.map((v, i) => (i !== keys.length - 1 ? `"${v}":{` : `"${v}":`)).join('') +
+              obj[k] +
+              '}'.repeat(keys.length)
+          )
+        );
+      } else acc[k] = obj[k];
+      return acc;
+    }, {})
   }
+  
 
 }
 
