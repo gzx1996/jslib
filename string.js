77a25a70fe36d4371f8f78705abc62d9b96a33bd
高振翔_time <xiangz.gao@timemicro.tech>
2020-09-09 11:22:14 +0800

    up

--- string.js
+++ string.js
@@ -3,7 +3,7 @@ module.exports = {
    * 获取字符串全排列
    * @param {string} str
    */
-  anagrams(str) {
+  strPermutations(str) {
     if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
     return str.split('').reduce((acc, letter, i) =>
       acc.concat(this.anagrams(str.slice(0, i) + str.slice(i + 1)).map(val => letter + val)), []);
