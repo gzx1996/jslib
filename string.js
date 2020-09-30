75eb55af0a944785ce6736589cc1c2c8af636469
高振翔_time <xiangz.gao@timemicro.tech>
2020-09-30 09:44:08 +0800

    up

--- string.js
+++ string.js
@@ -7,5 +7,15 @@ module.exports = {
     if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
     return str.split('').reduce((acc, letter, i) =>
       acc.concat(this.anagrams(str.slice(0, i) + str.slice(i + 1)).map(val => letter + val)), []);
+  },
+  /**
+   * 
+   * @param {string} cc 需要处理的字符串
+   * @param {number} startNum 开头留几个
+   * @param {number} endNum 结尾几个
+   * @param {string} mask 替换字符
+   */
+  mask(cc, startNum, endNum, mask = '*'){
+    return `${cc}`.slice(0, startNum) + ''.padStart(`${cc}`.length - startNum - endNum, mask) + `${cc}`.slice(-endNum);
   }
 }
--- test.js
+++ test.js
@@ -1,19 +1,2 @@
 let func = require('./index')
 
-
-let arr = [
-  {
-    name: '大头儿子',
-    father: '小头爸爸'
-  }, 
-  {
-    name: '小头爸爸',
-    father: '中头爷爷'
-  }, 
-  {
-    name: '中头爷爷',
-    father: null
-  }
-]
-let res =  func.buildTree(arr, 'name', 'father')
-console.log(JSON.stringify(res))
