fcfe3b213a4c0554f1f76b9e242b542d8aeb6dfc
高振翔_time <xiangz.gao@timemicro.tech>
2021-06-08 17:12:41 +0800

    up

--- test.js
+++ test.js
@@ -1,4 +1,38 @@
 let func = require('./index')
 
 
-console.log(func.time.format(new Date(), 'YYYY-MM-DD HH:mm:ss'));
+let arr = [{
+    id: 1,
+    name: '电单车',
+    code: 'ebike',
+    parentId: null
+}, {
+    id: 2,
+    name: '车篮',
+    code: 'bucket',
+    parentId: 1
+}, {
+    id: 3,
+    name: '脚踏板',
+    code: 'tt',
+    parentId: 1
+}, {
+    id: 4,
+    name: '螺丝',
+    code: 'll',
+    parentId: 3
+}, {
+    id: 5,
+    name: '自行车',
+    code: 'bike',
+    parentId: null
+}, {
+    id: 6,
+    name: '坐垫',
+    code: 'cushion',
+    parentId: 5
+}]
+
+let res = func.array.buildTree(arr, 'id', 'parentId');
+
+console.log(JSON.stringify(res))
